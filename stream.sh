95ec55757ba3ca7defffa19fc6f73b3f0fa5e20c
MetalArend <metalarend@gmail.com>
2016-07-08 11:40:45 +0200

    make the stream start more flexible

--- stream.sh
+++ stream.sh
@@ -3,6 +3,23 @@
 docker-compose stop && docker-compose rm -f && docker-compose build && docker-compose up -d --remove-orphans
 sleep 1
 
-brew install ffmpeg
-#ffmpeg -f avfoundation -list_devices true -i ""
-ffmpeg -f avfoundation -i :"Built-in Input" -c:a libmp3lame -b:a 320k -legacy_icecast 1 -content_type audio/mpeg -ice_name "Local Radio" -ice_description "Local Radio" -f mp3 icecast://source:changeme@localhost:8000/stream
+ICECAST_NAME=$(docker-compose exec icecast env | grep "ICECAST_NAME=" | sed 's/.*=//' | tr -d '[:cntrl:]')
+ICECAST_DESCRIPTION=$(docker-compose exec icecast env | grep "ICECAST_DESCRIPTION=" | sed 's/.*=//' | tr -d '[:cntrl:]')
+ICECAST_SOURCE_PASSWORD=$(docker-compose exec icecast env | grep "ICECAST_SOURCE_PASSWORD=" | sed 's/.*=//' | tr -d '[:cntrl:]')
+
+ICECAST_HOMEPAGE_URL="http://$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep '192'):8000"
+ICECAST_STREAM_URL="http://$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep '192'):8000/stream"
+(sleep 2; open "${ICECAST_HOMEPAGE_URL}"; open "${ICECAST_STREAM_URL}")&
+
+DEVICE=$(ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | sed -n -e '/audio devices/,$p' | grep -o "Built-in.*")
+
+brew instal ffmpeg
+ffmpeg -f avfoundation -i :"${DEVICE}" \
+        -c:a libmp3lame \
+        -b:a 320k \
+        -legacy_icecast 1 \
+        -content_type audio/mpeg \
+        -f mp3 \
+        -ice_name "${ICECAST_NAME}" \
+        -ice_description "${ICECAST_DESCRIPTION}" \
+        icecast://source:${ICECAST_SOURCE_PASSWORD}@localhost:8000/stream
